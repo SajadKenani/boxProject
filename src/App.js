53fa32de4417979d33c4715405f9dbbdd3fd78eb
SajadKenani <114795074+SajadKenani@users.noreply.github.com>
2023-12-21 12:53:36 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import {UpperContent} from "./upperContent.js";
+import {MiddleContent} from "./middleContent.js";
+
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <UpperContent />
+    <MiddleContent />
+ 
+    </div>
+  );
+}
+
+export default App;
+export const paddingLR = 10;
