be7aa4c17413314ab47d7fb6902947ddfb19d66d
SajadKenani <114795074+SajadKenani@users.noreply.github.com>
2023-12-25 09:30:13 +0300

    Update upperContent.js

--- src/upperContent.js
+++ src/upperContent.js
@@ -1,14 +1,43 @@
 import "./upperContent.css"
-import Logo from "./images/Logo.png"
+import Logo from "./images/Logo.webp"
 
-import menu from "./images/menu.png"
+import menu from "./images/menu.webp"
 
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 export const UpperContent = () => {
 
     const [myPadding, setmypadding] = useState(window.innerWidth > 640 ? 10 : 2);
     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const [activatedmenu, useactivatedmenu] = useState(false);
+
+    const barRef = useRef();
+    const shadowRef = useRef();
+
+    const  activateMenu = () => {
+
+      if (barRef.current){
+        barRef.current.classList.replace("hide-bar", "side-bar");}
+ 
+      if (shadowRef.current){
+        shadowRef.current.classList.replace("hide-shadow", "shadow-div");}
+
+
+      setTimeout(() => useactivatedmenu(!activatedmenu), 0);
+      
+
+    }
+
+    const leaveBar = () => {
+      if (barRef.current){
+        barRef.current.classList.replace("side-bar", "hide-bar");}
+ 
+      if (shadowRef.current){
+        shadowRef.current.classList.replace("shadow-div", "hide-shadow");}
+
+      setTimeout(() => useactivatedmenu(!activatedmenu), 490);
+    }
   
   // Function to handle window resize
   const handleResize = () => {
@@ -32,12 +61,33 @@ export const UpperContent = () => {
   }, [])
   
 
-    
 
     return (
         <div className=" fixed" style={{ backgroundColor:"#F5FCFF", zIndex: "14343222", width: "100%", paddingLeft: myPadding+"%", paddingRight:  myPadding+"%"}}>
-            <nav class="p-4">
-            <div class="container mx-auto flex justify-between items-center">
+
+        {activatedmenu && 
+          <div>
+            <div className="fixed bg-white side-bar bar pt-10 p-6 " ref={barRef} style={{height: "100%", left: "0", zIndex: "321"}}>
+              <h1 className="text-end text-2xl"> القائمة الرئــيسية </h1>
+
+              <div className="grid pt-4">
+                <a className="text-end pt-3"> أتصل بنا </a>
+                <a className="text-end pt-3"> أراء العملاء </a>
+                <a className="text-end pt-3"> الخدمات </a>
+                <a className="text-end pt-3"> مشاريعنا </a>
+                <a className="text-end pt-3"> عن الشركة </a>
+
+                <a className="text-end pt-6" style={{color: "#2997D8"}}> التواصل مع المبيعات </a>
+              </div>
+
+            </div>
+            <div className="fixed shadow-div shadow" ref={shadowRef} onClick={() => leaveBar()} style={{width: "100%", height: "100%", right: "0", backgroundColor: "rgba(0, 0, 0, 0.226)"}}></div>
+          </div>
+        }
+
+
+        <nav class="p-4">
+        <div class="container mx-auto flex justify-between items-center">
     
         
 
@@ -54,7 +104,7 @@ export const UpperContent = () => {
             
         </div>
 
-        <div id="mobile-menu" class="lg:hidden w-16 p-4"><img src={menu}/></div> 
+        <div id="mobile-menu" class="lg:hidden w-16 p-4" onClick={() => activateMenu()}><img src={menu}/></div> 
         <div>
             <img href="#" src={Logo} className="w-44 h-8"/>
         </div>
