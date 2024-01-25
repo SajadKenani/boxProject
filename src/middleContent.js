b4121f0514a272f2b3a09a96b32c87095b91086c
SajadKenani <114795074+SajadKenani@users.noreply.github.com>
2024-01-25 05:06:58 +0300

    Update middleContent.js

--- src/middleContent.js
+++ src/middleContent.js
@@ -496,7 +496,7 @@ export const MiddleContent = () => {
               <div className="flex justify-center mb-20 md:mb-0 order-1 lg:order-0">
               <div className="flex">
               <img style={{height: "56px", cursor: "pointer"}} className=" p-5 pt-0 pb-0 -m-3 mt-0 lg:m-5 sm:m-0 " src={whatsapp_Icon} onClick={() => window.location.assign("https://wa.me/+9647858002915")}/>
-              <a href="sms:+9647858002915?&body=Hi%2520there%252C%2520I%2527d%2520like%2520to%2520place%2520an%2520order%2520for..." className="p-5 pt-0 pb-0 -m-3 mt-0 lg:m-5 sm:m-0">
+              <a href="sms:+9647858002915?&body=" className="p-5 pt-0 pb-0 -m-3 mt-0 lg:m-5 sm:m-0">
                <img style={{ height: "56px", cursor: "pointer" }}  src={phonecall_Icon} alt="Email Agent" />
               </a>
               <a href="mailto:inboxcomp@hotmail.com?subject=From%InBox%Website" className=" p-5 pt-0 pb-0 -m-3 mt-0 lg:m-5 sm:m-0">
